760149b8852da074f62656d9b2bf850973c4e0e8
Rory Demers <roryjackd@yahoo.com>
2019-09-05 13:40:17 -0600

    Refactored post create.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: [:show]
 
+  def index
   end
 
   def new
@@ -8,14 +9,25 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post = Post.new(post_params)
 
-    @post.save
-
-    redirect_to @post
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end
