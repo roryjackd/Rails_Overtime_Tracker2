ec7422b1ffffca3a7ba2b8fcdb5a99ae4d6a25b0
Rory Demers <roryjackd@yahoo.com>
2019-09-17 13:34:09 -0600

    Refactoring for factorygirl.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    @user = FactoryGirl.create(:user)
   end
 
   describe "creation" do
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
 
   describe "custom name methods" do
     it "has a full name method that combines first and last name" do
-      expect(@user.full_name).to eq("SNOW, JON")
+      expect(@user.full_name).to eq("DEMERS, RORY")
     end
   end
 end
