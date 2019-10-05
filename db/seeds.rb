08e621d270d67ee3232345340fddc18af9d8dd0b
Rory Demers <roryjackd@yahoo.com>
2019-10-05 11:38:41 -0600

    Dashboard show approval status.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 User created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Demers")
+
+puts "1 User created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
