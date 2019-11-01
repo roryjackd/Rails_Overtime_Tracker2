03b3b7787d3c50cad3414e9c4a8a53e4aa128a2c
Rory Demers <roryjackd@yahoo.com>
2019-11-01 14:10:44 -0600

    Generated audit log feature.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_172312) do
+ActiveRecord::Schema.define(version: 2019_11_01_184502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auditlogs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "status", default: 0
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_auditlogs_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -42,5 +52,6 @@ ActiveRecord::Schema.define(version: 2019_10_24_172312) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "auditlogs", "users"
   add_foreign_key "posts", "users"
 end
---
+++ spec/factories/auditlogs.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :auditlog do
+    user nil
+    status 1
+    start_date "2019-11-01"
+    end_date "2019-11-01"
+  end
+end
