1f6840c566eb7c66a20c66d3200a27dabd0347ba
Rory Demers <roryjackd@yahoo.com>
2019-11-02 12:18:40 -0600

    Created factory for audit log.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ puts "1 User created"
 end
 
 puts "100 Posts have been created"
+
+100.times do |post|
+  Auditlog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
--- spec/factories/auditlogs.rb
+++ spec/factories/auditlogs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :auditlog do
-    user nil
-    status 1
-    start_date "2019-11-01"
-    end_date "2019-11-01"
+    user
+    status 0
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
