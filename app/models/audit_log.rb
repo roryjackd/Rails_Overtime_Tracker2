921ca43df0688fac4aaacda16e2dad4c844e5b98
Rory Demers <roryjackd@yahoo.com>
2019-11-12 13:15:07 -0700

    Status enum and label for audit items.

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,15 @@
 class AuditLog < ApplicationRecord
+  enum status: { pending: 0, confirmed: 1}
+
   belongs_to :user
+
+  validates_presence_of :user_id, :status, :start_date
+
+  after_initialize :set_defaults
+
+  private
+
+    def set_defaults
+      self.start_date ||= Date.today -6.days
+    end
 end
